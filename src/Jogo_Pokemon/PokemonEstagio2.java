8174a54859dbc17244d458f08e329c414ab05d72
carlosbonifacio.sousa@gmail.com <carlosbonifacio.sousa@gmail.com>
2022-11-26 18:19:04 -0300

    Atualização de métodos inserção da escolha inicial

--- src/Jogo_Pokemon/PokemonEstagio2.java
+++ src/Jogo_Pokemon/PokemonEstagio2.java
@@ -2,4 +2,11 @@ package Jogo_Pokemon;
 
 public class PokemonEstagio2 extends Pokemon {
     
+    PokemonEstagio2(String nome){
+        this.nome = nome;
+        estagio = 2;
+        level = getRandomLevel();
+        Treinador.Pokemons_Capturados.add(this);
+    }
+
 }
